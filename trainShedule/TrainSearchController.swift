71cf95f280e203e83a9de7f47222d5014e13be23
Panyushenko <>
2018-04-09 23:44:00 +0300

    configure train timetable manager view controller(create constraints)

---
+++ trainShedule/TrainSearchController.swift
@@ -0,0 +1,23 @@
+//
+//  TrainSearchController.swift
+//  trainShedule
+//
+//  Created by Panyushenko on 09.04.2018.
+//  Copyright © 2018 Artyom Panyushenko. All rights reserved.
+//
+
+import UIKit
+
+class TrainSearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
