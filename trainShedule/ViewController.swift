c740b3a2902f4948889f79fb95422dfe6543b33a
Panyushenko <>
2018-04-09 15:04:11 +0300

    empty project

---
+++ trainShedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trainShedule
+//
+//  Created by Panyushenko on 09.04.2018.
+//  Copyright © 2018 Artyom Panyushenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
